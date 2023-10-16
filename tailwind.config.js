7131dfbb49ce9f07011e091b77433aec69c2b7fa
lucianamendezg <33171331+lucianamendezg@users.noreply.github.com>
2023-10-15 20:50:45 -0600

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}',],
+  theme: {
+    colors: {
+      'blue':'#1D22DC',
+      'pink': '#B96493',
+      'red': '#C55042',
+      'orange': '#E28E25'
+    },
+    extend: {
+      fontFamily: {
+        agoraSans: ['agora-sans'],
+        filson: ['filson'],
+        agoraSlab: ['agora-slab'],
+      },
+      colors: {
+        'light-blue': '#5C7DF3'
+      }
+    },
+  },
+  plugins: [],
+}
+
