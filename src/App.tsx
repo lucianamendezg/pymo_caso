ef57b4126c7a4397e64d2413d16e1e83b9f384db
lucianamendezg <33171331+lucianamendezg@users.noreply.github.com>
2023-10-17 22:32:49 -0600

    initial footer created

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Header } from './components/layout/header'
+import { Footer } from './components/layout/footer'
 
 function App() {
   return (
     <div>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,15 @@
+
+export const Footer = () => {
+    return (
+        <div className="bg-white py-16 border-t-2">
+            <a href="mailto:luciana.mendezg@gmail.com">
+                <h2 className="text-orange font-filson text-lg md:text-3xl pl-6 md:pl-12 hover:text-red">
+                    ¡Comunicate conmigo!
+                </h2>
+                <p className="text-right font-filson text-xs md:text-sm p-6">
+                    Creado utilizando React, Typescript, Tailwind CSS, AWS, y mucho ☕ 
+                </p>
+            </a>
+        </div>
+    )
+}
