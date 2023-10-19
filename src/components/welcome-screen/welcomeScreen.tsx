57d0a19e39ed1e01a505d7ab5790f15a13c371f4
lucianamendezg <33171331+lucianamendezg@users.noreply.github.com>
2023-10-19 08:26:13 -0600

    Welcome screen text + image

--- src/components/welcome-screen/welcomeScreen.tsx
+++ src/components/welcome-screen/welcomeScreen.tsx
@@ -1,13 +1,14 @@
 import HeroImage from "../layout/heroImage"
 import Layout from "../layout/layout"
+import luciana from "../../images/luciana-image.jpg"
 
 export default function WelcomeScreen () {
     return (
         <Layout>
             <HeroImage
-                title={'Welcome Screen'}
-                subtitle={'Subtitle'}
-                image=''
+                title={'Hola, Soy Luciana'}
+                subtitle={'Gracias por considerarme como candidata. Los entregables están en el menú. ¡Espero me la haya rifado!'}
+                image={luciana}
             />
         </Layout>
     )
