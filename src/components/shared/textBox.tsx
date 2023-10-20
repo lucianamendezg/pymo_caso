4fa42d913e9556b6ab8cdea298aca629a7a867bf
lucianamendezg <33171331+lucianamendezg@users.noreply.github.com>
2023-10-19 22:20:01 -0600

    added errors, has bug

--- src/components/shared/textBox.tsx
+++ src/components/shared/textBox.tsx
@@ -1,15 +1,28 @@
 interface Props {
     textLabel: string,
     id: string
+    onChange?:any;
 }
 
 export const TextBox = (props:Props) => {
     return (
         <div className="">
-            <label className="block font-filson font-normal text-white" id={props.id}>
+            <label className="block font-filson font-normal text-white">
                 {props.textLabel}
             </label>
-            <input className="rounded shadow-md my-4 md:w-96" required>
+            <input className="rounded shadow-md my-4 md:w-96" onChange={props.onChange} id={props.id} required>
+            </input>
+        </div>
+    )
+}
+
+export const TextBoxError = (props:Props) => {
+    return (
+        <div className="">
+            <label className="block font-filson font-normal text-white">
+                {props.textLabel}
+            </label>
+            <input className="rounded shadow-md my-4 md:w-96 border-4 border-red" placeholder="Error: ingrese un número" onChange={props.onChange} id={props.id} required>
             </input>
         </div>
     )
